f627661edf602ab95b88d07a9418d560d534ecb7
Outa Valkama <outa@viklo.fi>
2017-03-22 11:59:35 +0200

    Merget newest changes, resolved conflicts

--- src/main/scala/fi/vm/sade/vst/repository/DBUserRepository.scala
+++ src/main/scala/fi/vm/sade/vst/repository/DBUserRepository.scala
@@ -46,12 +46,12 @@ class DBUserRepository(val config: DBConfig) extends UserRepository with Session
   private def updateUserProfile(userId: String, userProfileUpdate: UserProfileUpdate) = {
     DB localTx { implicit session =>
       withSQL {
-        update(UserProfileTable).set(u.sendEmail -> userProfileUpdate.sendEmail).where.eq(u.uid,uid)
+        update(UserProfileTable).set(u.sendEmail -> userProfileUpdate.sendEmail).where.eq(u.userId, userId)
       }.update().apply()
       withSQL {
-        delete.from(UserCategoryTable).where.eq(uc.userId,userId)
+        delete.from(UserCategoryTable).where.eq(uc.userId, userId)
       }.update().apply()
-      userProfileUpdate.categories.foreach(id => insertUserCategory(userId,id))
+      userProfileUpdate.categories.foreach(id => insertUserCategory(userId, id))
     }
     UserProfile(userId, userProfileUpdate.categories, userProfileUpdate.sendEmail)
   }
