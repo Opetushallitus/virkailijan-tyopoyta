28a10f42332fba2496d353ea50809ba71530899d
Mikko Tyrväinen <mikko.tyrvainen@viklo.fi>
2017-04-25 09:32:44 +0300

    Url properties, default logback conffi.

--- src/main/scala/fi/vm/sade/vst/security/AuthenticationModule.scala
+++ src/main/scala/fi/vm/sade/vst/security/AuthenticationModule.scala
@@ -4,14 +4,13 @@ import fi.vm.sade.security.ldap.LdapClient
 import fi.vm.sade.utils.cas._
 import fi.vm.sade.vst.Configuration
 import fi.vm.sade.vst.repository.RepositoryModule
-import fi.vm.sade.vst.service.EmailService
 import org.http4s.client
 
 trait AuthenticationModule extends Configuration with RepositoryModule{
 
   import com.softwaremill.macwire._
 
-  lazy val casClient = new CasClient(authenticationConfig.casUrl, client.blaze.defaultClient)
+  lazy val casClient = new CasClient(urls.url("cas.url"), client.blaze.defaultClient)
 
   lazy val casUtils: CasUtils = wire[CasUtils]
   lazy val ldapClient: LdapClient = wire[LdapClient]
