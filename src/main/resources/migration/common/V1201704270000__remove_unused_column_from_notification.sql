67b6e8b12bee1c83fc9ada5dbf000f9805228357
Mikko Tyrväinen <mikko.tyrvainen@viklo.fi>
2017-04-27 13:44:29 +0300

    Korjattu kantamigraatio

--- src/main/resources/migration/common/V1201704270000__remove_unused_column_from_notification.sql
+++ src/main/resources/migration/common/V1201704270000__remove_unused_column_from_notification.sql
@@ -1,6 +1,2 @@
 -- Reset id sequence
-SELECT setval('tag_id_seq', (SELECT MAX(id) FROM tag));
-
-INSERT INTO tag (name, group_id) VALUES ('Yhteishaku', 1);
-INSERT INTO tag (name, group_id) VALUES ('Lisähaku', 1);
-
+ALTER TABLE notification_content DROP COLUMN timeline_text;
