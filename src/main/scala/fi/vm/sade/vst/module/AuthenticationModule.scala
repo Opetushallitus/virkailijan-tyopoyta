9815e0e3197edba4d551017004b53d1957942eca
Outa Valkama <outa@viklo.fi>
2017-04-28 10:43:35 +0300

    Newest changes merged.

--- src/main/scala/fi/vm/sade/vst/module/AuthenticationModule.scala
+++ src/main/scala/fi/vm/sade/vst/module/AuthenticationModule.scala
@@ -3,7 +3,6 @@ package fi.vm.sade.vst.module
 import fi.vm.sade.security.ldap.LdapClient
 import fi.vm.sade.utils.cas._
 import fi.vm.sade.vst.Configuration
-import fi.vm.sade.vst.repository.RepositoryModule
 import fi.vm.sade.vst.security.CasUtils
 import org.http4s.client
 
