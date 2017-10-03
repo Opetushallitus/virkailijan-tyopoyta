48d9a8060f0654968813f8dc7beeb95c10071e90
teo.mertanen <teo.mertanen@gofore.com>
2017-10-03 13:52:40 +0300

    add access log library

--- src/main/scala/fi/vm/sade/vst/server/Server.scala
+++ src/main/scala/fi/vm/sade/vst/server/Server.scala
@@ -1,5 +1,11 @@
 package fi.vm.sade.vst.server
 
+import akka.Done
+import akka.event.{Logging, LoggingAdapter}
+import akka.stream.scaladsl.Sink
+import de.heikoseeberger.accessus.Accessus._
+
+import scala.concurrent.Future
 import akka.actor.ActorSystem
 import akka.dispatch.MessageDispatcher
 import akka.http.scaladsl.Http
@@ -14,8 +20,20 @@ class Server(routes: Routes, config: ServerConfig, implicit val system: ActorSys
 
   private lazy val port = config.port
 
+  private def accessLog(accessLogger: LoggingAdapter): AccessLog[Long, Future[Done]] =
+    Sink.foreach {
+      case ((req, t0), res) =>
+        val m = req.method.value
+        val p = req.uri.path.toString
+        val s = res.status.intValue()
+        val t = (System.nanoTime() - t0) / 1000
+        accessLogger.info(s"$m $p $s $t")
+    }
+
+  private val route: Handler[Future[Done]] = routes.routes.withAccessLog(accessLog(Logging(system, "ACCESS_LOG")))
+
   def start(): Unit = {
-    val handler = Http().bindAndHandle(routes.routes, "0.0.0.0", port)
+    val handler = Http().bindAndHandle(route, "0.0.0.0", port)
     logger.info(s"Starting server on port $port")
     handler.failed.foreach { case ex: Exception => logger.error("Failed to bind to port", ex) }
   }
