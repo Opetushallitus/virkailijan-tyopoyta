ea6e129d4c59c843748f52bcdff83f96c831b7ed
teo.mertanen <teo.mertanen@gofore.com>
2018-02-23 13:43:02 +0200

    OY-168 migraatio: tagit Neuvonta ja Muut kantaan

---
+++ src/main/resources/migration/postgresql/V1201802230000__add_tags_neuvonta_muut.sql
@@ -0,0 +1,8 @@
+-- OY-168
+-- neuvonta/ rådgivning --> perusopetus, toisen asteen alle
+-- muut/ övriga --> kaikkien alle eli perusopetus, toinen aste, korkea-aste, Koski, vapaa sivistyö
+
+INSERT INTO tag (name, group_id) VALUES ('Neuvonta', 1);
+INSERT INTO tag (name, group_id) VALUES ('Muut', 1);
+INSERT INTO tag (name, group_id) VALUES ('Muut', 2);
+INSERT INTO tag (name, group_id) VALUES ('Muut', 3);
