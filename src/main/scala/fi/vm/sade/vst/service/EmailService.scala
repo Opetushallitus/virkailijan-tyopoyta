aa17424a5dd5d7063cd04e5f4d467aabc2cbd3ab
teo.mertanen <teo.mertanen@gofore.com>
2018-07-27 15:06:13 +0300

    BUG-1710 reorganize & refactor EmailService so it's more readable

--- src/main/scala/fi/vm/sade/vst/service/EmailService.scala
+++ src/main/scala/fi/vm/sade/vst/service/EmailService.scala
@@ -19,11 +19,11 @@ class EmailService(casUtils: CasUtils,
                    val accessService: KayttooikeusService,
                    val userService: UserService)
   extends RepositoryModule
-  with GroupEmailComponent
-  with Configuration
-  with JsonFormats
-  with LazyLogging
-  with JsonSupport {
+    with GroupEmailComponent
+    with Configuration
+    with JsonFormats
+    with LazyLogging
+    with JsonSupport {
 
   sealed trait EmailEventType {
     val description: String
@@ -46,31 +46,19 @@ class EmailService(casUtils: CasUtils,
   lazy val groupEmailService: GroupEmailService = new RemoteGroupEmailService(emailConfiguration, "virkailijan-tyopoyta-emailer")
 
   private def oppijanumeroRekisteri = casUtils.serviceClient(oppijanumeroRekisteriConfig.serviceAddress)
+
   private def userAccessService = casUtils.serviceClient(urls.url("kayttooikeus-service.url"))
 
   implicit val localDateOrdering: Ordering[LocalDate] = Ordering.by(_.toEpochDay)
   private val dateTimeFormat: DateTimeFormatter = java.time.format.DateTimeFormatter.ofPattern("dd.MM.yyyy")
 
-  private def parseUserInformationFromResponse(response: String): Seq[UserInformation] = {
-    val json = Json.parse(response).asOpt[JsArray].map(_.value).getOrElse(Seq.empty)
-    val userInformation = json.flatMap(parseSingleUserInformation)
-    userInformation
-  }
-
-  private def parsePersonOidsFromResponse(response: String): Seq[String] = {
-    val json = Json.parse(response)
-    val personOids = (json \ "personOids").asOpt[Seq[String]].getOrElse(Seq.empty)
-    personOids
-  }
-
-  private def parseUserOidsAndEmails(resp: Try[String]): Seq[UserInformation] = {
-    resp match {
-      case Success(s) =>
-        parseUserInformationFromResponse(s)
-      case Failure(f) =>
-        logger.error("Failed to parse user oids and emails", f)
-        Seq.empty
-    }
+  def sendEmailsForDate(date: LocalDate)(implicit au: AuditUser): Unit = {
+    // TODO: Should this just take range of dates? At the moment it is easier to just get evets for current and previous date
+    logger.info(s"Preparing to send emails for date $date")
+    val releases = releaseRepository.getEmailReleasesForDate(date)
+    val previousDateReleases = releaseRepository.getEmailReleasesForDate(date.minusDays(1))
+    val results: Seq[String] = sendEmails(releases ++ previousDateReleases, TimedEmail)
+    logger.info("sendEmailsForDate result: " + results.mkString(", "))
   }
 
   def sendEmails(releases: Seq[Release], eventType: EmailEventType)(implicit au: AuditUser): Seq[String] = {
@@ -80,25 +68,78 @@ class EmailService(casUtils: CasUtils,
 
     val result = releaseSetsForUsers.flatMap {
       case (userInfo, releasesForUser) =>
-        val recipients = List(formRecipient(userInfo.email))
+        val recipient = EmailRecipient(userInfo.email)
         val emailMessage = formEmail(userInfo, releasesForUser)
-        groupEmailService.sendMailWithoutTemplate(EmailData(emailMessage, recipients))
+        groupEmailService.sendMailWithoutTemplate(EmailData(emailMessage, List(recipient)))
     }
 
-    addEmailEvents(releases.map(releaseToEmailEvent(_, eventType)))
+    addEmailEvents(releases, eventType)
     result
   }
+
 
   private def getUsersToReleaseSets(releases: Seq[Release]): Seq[(BasicUserInformation, Set[Release])] = {
     val userReleasePairs: Seq[(BasicUserInformation, Release)] = releases.flatMap { release =>
       val userGroups: Set[Long] = userGroupIdsForRelease(release)
-      val users: Seq[BasicUserInformation] = getUserInformationsForGroups(userGroups)
-      val filteredUsers: Seq[BasicUserInformation] = filterUsersForReleases(release, users)
+      val usersForGroups: Seq[BasicUserInformation] = getUserInformationsForGroups(userGroups)
+      val filteredUsers: Seq[BasicUserInformation] = filterUsersForReleases(release, usersForGroups)
       filteredUsers.map(_ -> release)
     }
     userReleasePairs.groupBy(_._1)
       .mapValues(_.map(_._2).toSet)
       .toSeq
+  }
+
+  private def userGroupIdsForRelease(release: Release): Set[Long] = {
+    val userGroupsForRelease = releaseRepository.userGroupsForRelease(release.id).map(_.usergroupId)
+    val virkailijanTyopoytaRoles: Seq[Long] = accessService.appGroups.map(_.id)
+    virkailijanTyopoytaRoles.intersect(userGroupsForRelease).toSet
+  }
+
+  private def getUserInformationsForGroups(userGroups: Set[Long]): Seq[BasicUserInformation] = {
+    val personOids = userGroups.flatMap(personOidsForUserGroup)
+    IterableUtils.mapToSplitted(450, personOids, getUserInformationsForOids).toSeq
+  }
+  private def personOidsForUserGroup(groupOid: Long): Seq[String] = {
+    def parsePersonOidsFromResponse(response: String): Seq[String] = {
+      val json = Json.parse(response)
+      val personOids = (json \ "personOids").asOpt[Seq[String]].getOrElse(Seq.empty)
+      personOids
+    }
+
+    val response = userAccessService.authenticatedRequest(urls.url("kayttooikeus-service.personOidsForUserGroup", groupOid.toString), RequestMethod.GET)
+    response match {
+      case Success(s) =>
+        parsePersonOidsFromResponse(s)
+      case Failure(f) =>
+        logger.error(s"Failure parsing person oids from response $response", f)
+        Seq.empty
+    }
+  }
+
+  private def getUserInformationsForOids(oids: Iterable[String]): Iterable[BasicUserInformation] = {
+    val formattedOids = oids.map { oid => s""""$oid"""" }
+    val json = s"""[${formattedOids.mkString(",")}]"""
+    val body = Option(json)
+
+    val url = s"${oppijanumeroRekisteriConfig.serviceAddress}/henkilo/henkilotByHenkiloOidList"
+    val response = oppijanumeroRekisteri.authenticatedRequest(url, RequestMethod.POST, mediaType = Option(org.http4s.MediaType.`application/json`), body = body)
+    val userInformation: Seq[UserInformation] = parseUserOidsAndEmails(response)
+    for {
+      userInfo <- userInformation
+      contactGroups <- userInfo.yhteystiedotRyhma.filter(_.ryhmaKuvaus == groupTypeFilter)
+      contactInfo <- contactGroups.yhteystieto.filter(_.yhteystietoTyyppi == contactTypeFilter)
+    } yield BasicUserInformation(userInfo.oidHenkilo, contactInfo.yhteystietoArvo, Seq(userInfo.asiointiKieli.kieliKoodi))
+  }
+
+  private def parseUserOidsAndEmails(resp: Try[String]): Seq[UserInformation] = {
+    resp match {
+      case Success(s) =>
+        parseUserInformationFromEmailResponse(s)
+      case Failure(f) =>
+        logger.error("Failed to parse user oids and emails", f)
+        Seq.empty
+    }
   }
 
   private def filterUsersForReleases(release: Release, users: Seq[BasicUserInformation]): Seq[BasicUserInformation] = {
@@ -133,14 +174,6 @@ class EmailService(casUtils: CasUtils,
     includedUsers
   }
 
-  def sendEmailsForDate(date: LocalDate)(implicit au: AuditUser): Unit = {
-    // TODO: Should this just take range of dates? At the moment it is easier to just get evets for current and previous date
-    logger.info(s"Preparing to send emails for date $date")
-    val releases = releaseRepository.getEmailReleasesForDate(date)
-    val previousDateReleases = releaseRepository.getEmailReleasesForDate(date.minusDays(1))
-    val results: Seq[String] = sendEmails(releases ++ previousDateReleases, TimedEmail)
-    logger.info("sendEmailsForDate result: " + results.mkString(", "))
-  }
 
   private def formEmail(user: BasicUserInformation, releases: Set[Release]): EmailMessage = {
     val language = user.languages.headOption.getOrElse("fi") // Defaults to fi if no language is found
@@ -157,52 +190,11 @@ class EmailService(casUtils: CasUtils,
     EmailMessage("virkailijan-tyopoyta", subject, EmailHtmlService.htmlString(releases, language), html = true)
   }
 
-  private def formRecipient(email: String): EmailRecipient = {
-    EmailRecipient(email)
-  }
 
-  private def userGroupIdsForRelease(release: Release): Set[Long] = {
-    val userGroupsForRelease = releaseRepository.userGroupsForRelease(release.id).map(_.usergroupId)
-    val virkailijanTyopoytaRoles: Seq[Long] = accessService.appGroups.map(_.id)
-    virkailijanTyopoytaRoles.intersect(userGroupsForRelease).toSet
-  }
+  private def addEmailEvents(releases: Seq[Release], eventType: EmailEventType)(implicit au: AuditUser): Seq[EmailEvent] = {
+    def releaseToEmailEvent(release: Release) = EmailEvent(0l, java.time.LocalDate.now(), release.id, eventType.description)
 
-  private def getUserInformationsForGroups(userGroups: Set[Long]): Seq[BasicUserInformation] = {
-    val personOids = userGroups.flatMap(personOidsForUserGroup)
-    IterableUtils.mapToSplitted(450, personOids, getUserInformationsForOids).toSeq
-  }
-
-  private def personOidsForUserGroup(groupOid: Long): Seq[String] = {
-    val response = userAccessService.authenticatedRequest(urls.url("kayttooikeus-service.personOidsForUserGroup", groupOid.toString), RequestMethod.GET)
-    response match {
-      case Success(s) =>
-        parsePersonOidsFromResponse(s)
-      case Failure(f) =>
-        logger.error(s"Failure parsing person oids from response $response", f)
-        Seq.empty
-    }
-  }
-
-  private def getUserInformationsForOids(oids: Iterable[String]): Iterable[BasicUserInformation] = {
-    val formattedOids = oids.map { oid => s""""$oid"""" }
-    val json = s"""[${formattedOids.mkString(",")}]"""
-    val body = Option(json)
-
-    val url = s"${oppijanumeroRekisteriConfig.serviceAddress}/henkilo/henkilotByHenkiloOidList"
-    val response = oppijanumeroRekisteri.authenticatedRequest(url, RequestMethod.POST, mediaType = Option(org.http4s.MediaType.`application/json`), body = body)
-    val userInformation: Seq[UserInformation] = parseUserOidsAndEmails(response)
-    for {
-      userInfo <- userInformation
-      contactGroups <- userInfo.yhteystiedotRyhma.filter(_.ryhmaKuvaus == groupTypeFilter)
-      contactInfo <- contactGroups.yhteystieto.filter(_.yhteystietoTyyppi == contactTypeFilter)
-    } yield BasicUserInformation(userInfo.oidHenkilo, contactInfo.yhteystietoArvo, Seq(userInfo.asiointiKieli.kieliKoodi))
-  }
-
-  private def releaseToEmailEvent(release: Release, eventType: EmailEventType): EmailEvent = {
-    EmailEvent(0l, java.time.LocalDate.now(), release.id, eventType.description)
-  }
-
-  private def addEmailEvents(emailEvents: Seq[EmailEvent])(implicit au: AuditUser): Seq[EmailEvent] = {
+    val emailEvents = releases.map(releaseToEmailEvent)
     emailEvents.flatMap(emailRepository.addEvent(_))
   }
 }
