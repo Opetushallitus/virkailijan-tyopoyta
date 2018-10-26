320e392dd7ec978ac415a28dc509379579f07cdc
teo.mertanen <teo.mertanen@gofore.com>
2018-10-26 12:56:03 +0300

    BUG-1859 set email timeout temporarily to 20 seconds for testing

--- src/main/scala/fi/vm/sade/vst/server/routes/EmailRoutes.scala
+++ src/main/scala/fi/vm/sade/vst/server/routes/EmailRoutes.scala
@@ -28,7 +28,7 @@ class EmailRoutes(val userService: UserService, releaseService: ReleaseService, 
     with ResponseUtils
     with LazyLogging {
 
-  val emailTimeout: FiniteDuration = 1.seconds
+  val emailTimeout: FiniteDuration = 20.seconds
 
   private def sendHtml[T](eventualResult: Future[T]): Route = {
     onComplete(eventualResult) {
