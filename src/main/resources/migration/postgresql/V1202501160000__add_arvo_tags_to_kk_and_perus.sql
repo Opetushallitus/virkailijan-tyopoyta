24b47ad1cde4ed73301516ba923db2d4bf3c09ab
Joona Haavisto <joona.haavisto@gofore.com>
2025-01-16 15:34:46 +0200

    Lisätään uusia avainsanoja

---
+++ src/main/resources/migration/postgresql/V1202501160000__add_arvo_tags_to_kk_and_perus.sql
@@ -0,0 +1,15 @@
+--- OY-4811
+-- Perusopetus ja toinen aste (id 1): Suullinen kielikoe, Puheviestintä, Lukiodiplomi, Demolomake, Koulutus/Klinikka
+-- Korkeakoulutus (id 2): Arvo
+-- Arvo-rajapintakäyttäjä -käyttöoikeusryhmä
+
+
+INSERT INTO tag (name, group_id) VALUES ('Suullinen kielikoe', 1);
+INSERT INTO tag (name, group_id) VALUES ('Puheviestintä', 1);
+INSERT INTO tag (name, group_id) VALUES ('Lukiodiplomi', 1);
+INSERT INTO tag (name, group_id) VALUES ('Demolomake', 1);
+INSERT INTO tag (name, group_id) VALUES ('Koulutus/Klinikka', 1);
+
+INSERT INTO tag (name, group_id) VALUES ('Arvo', 2);
+
+
